c02281287e2b9a2c32550214aee530be8fe1b305
shaun <smccreight154@gmail.com>
2021-08-20 02:19:44 +0000

    created an assertequal function

--- assertequal.js
+++ assertequal.js
@@ -1,12 +1,28 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+// const sum = function(a, b) { 
+//   return a + b;
+// }
 
-  if (actual === expected) {
-    console.log('😄 ' + actual + " does equal " + expected);
-  } else {
-    console.log('😥 ' + actual + " does not " + expected);
-  }
+// // TEST CODE
+//  console.assert(sum(1, 2) === 3);
+// console.assert(sum(1, 1) === 3);
+
+
+
+
+const assertEqual = function(actual, expected) {
+ if(actual === expected){
+   console.log()
+   //left blank cause assertions do not log anything if truthy other code would be --> (code below)
+   // console.log("😄😄😄 --ASSERTION PASSED!--:" + actual + " does equal " + expected);
+ }else{
+   console.log("😟😟😟 --ASSERTION FAILED!--:" + actual + " does NOT equal " + expected);
+ }
+ 
+
 };
 
-// console.assert(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.assert(assertEqual(1, 1));
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+//  assertEqual(1, 1);
+
+
