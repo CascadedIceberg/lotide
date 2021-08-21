6976e649a1f9c1d2e752fbae65e08c05697b17cf
shaun <smccreight154@gmail.com>
2021-08-21 21:14:31 +0000

    oejgoejg

---
+++ findkey.js
@@ -0,0 +1,25 @@
+
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
+  
+    console.log(`😄😄😄 --ASSERTION PASSED!--: ${actual} does equal ${expected}`);
+  }else{
+    console.log(`😟😟😟 --ASSERTION FAILED!--: ${actual} does NOT equal ${expected}`);
+  }
+  
+ 
+ };
+
+ const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) return key;
+  }
+};
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
